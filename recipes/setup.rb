8e486e3da487136867d70b3c915a6e1864a46153
devopskce <devopskce@gmail.com>
2019-04-09 02:48:04 +0100

    Initial commit

---
+++ recipes/setup.rb
@@ -0,0 +1,16 @@
+package 'git' do
+action :install
+end
+package 'tree' do
+action :install
+end
+package 'ntp' do
+action :install
+end
+file '/etc/motd' do
+content 'Property of ...'
+owner 'root'
+group 'root'
+end
+
+
